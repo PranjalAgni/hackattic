2c6e48d5434bf48f4d580fa1c693ee304003167f
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-09-13 23:59:36 +0530

    :sparkles: playing with eslint

--- test.ts
+++ test.ts
@@ -1,17 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const sleep = async (delay: number) => {
-  // eslint-disable-next-line @typescript-eslint/return-await
-  return new Promise((resolve) => {
-    console.log("Waiting...");
-    setTimeout(() => {
-      resolve("Hey I slept");
-    }, delay);
-  });
-};
+async function performSomeWorkAsync(x: number, y: number): Promise<void> {
+  await (x + y);
+}
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const main = () => {
-  return sleep(2000);
-};
-
-main();
+performSomeWorkAsync(2, 4).then().catch();
