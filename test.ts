d1f9c05f9e16b84ebb434b27cbcece26d7a646d9
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-09-14 00:48:34 +0530

    :hammer: debugging typescript inside vscode with custom launch.json

--- test.ts
+++ test.ts
@@ -1,5 +1,17 @@
-async function performSomeWorkAsync(x: number, y: number): Promise<void> {
-  await (x + y);
+/* eslint-disable no-console */
+async function resolveAfter2Seconds(): Promise<string> {
+  return await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("resolved");
+    }, 2000);
+  });
 }
 
-performSomeWorkAsync(2, 4).then().catch();
+async function asyncCall(): Promise<void> {
+  console.log("calling");
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+  // expected output: "resolved"
+}
+
+asyncCall().then(console.log).catch(console.error);
