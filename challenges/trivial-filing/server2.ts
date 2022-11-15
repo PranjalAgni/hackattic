45d73a267374dcf58eea68c6997dc1de396f3c50
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-11-16 01:18:27 +0530

    :sparkles: tried fs.readFileSync

--- challenges/trivial-filing/server2.ts
+++ challenges/trivial-filing/server2.ts
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import tftp from "tftp2";
 import debug from "debug";
+import path from "path";
+import fs from "fs";
 
 const logger = debug("hackattic:trivial-filing");
+const dataDirectory = path.join(__dirname, "data");
 
 // This is simple tftp server which handles the GET/PUT of files
 export const createAndRunTftpServer = (host: string, port: number) => {
@@ -10,7 +13,8 @@ export const createAndRunTftpServer = (host: string, port: number) => {
   server.on("get", async (req: any, send: any) => {
     const { filename } = req;
     logger("Request arrived for ", filename);
-    await send(Buffer.from("hello world", "utf-8"));
+    await send(fs.readFileSync(path.resolve(dataDirectory, filename)));
+    logger("Sent data back");
   });
 
   server.listen(port);
