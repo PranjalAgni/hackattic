71b928d31f33bdb35d7e544c51ab6fead32f5b55
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-11-30 00:05:07 +0530

    :sparkles: reading password inmemory

--- challenges/brute-force-zip/index.ts
+++ challenges/brute-force-zip/index.ts
@@ -1,5 +1,9 @@
 import debug from "debug";
 import fetch from "node-fetch";
+import fs from "fs";
+import path from "path";
+import LineReader from "n-readlines";
+
 import { IData } from "./interface";
 
 const logger = debug("hackattic:brute-force-zip");
@@ -8,9 +12,13 @@ export const solver = async (
   problemUrl: string,
   submissionUrl: string
 ): Promise<void> => {
+  const passwordList = getPasswordsList(path.join(__dirname, "rockyou.txt"));
+  logger("Total Passwords: ", passwordList.length);
   const data = await fetchInput(problemUrl);
   if (data === null) return;
   logger("Zip package: ", data.zip_url);
+  const zipPath = path.join(__dirname, "store", "file.zip");
+  await downloadFile(data.zip_url, zipPath);
 };
 
 const fetchInput = async (problemUrl: string): Promise<IData | null> => {
@@ -24,19 +32,41 @@ const fetchInput = async (problemUrl: string): Promise<IData | null> => {
   return null;
 };
 
-const sendOutput = async (
-  submissionUrl: string,
-  message: unknown
-): Promise<string> => {
-  const response = await fetch(submissionUrl, {
-    method: "POST",
-    headers: {
-      Accept: "application.json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(message)
-  });
+const getPasswordsList = (passwordFile: string) => {
+  const passwordList = [];
+  const lineReader = new LineReader(passwordFile);
+  let line = lineReader.next();
+  while (line !== null) {
+    passwordList.push(line.toString("utf-8"));
+    line = lineReader.next();
+  }
 
-  const res = await response.text();
-  return res;
+  return passwordList;
 };
+
+const downloadFile = async (zipUrl: string, path: string) => {
+  const res = await fetch(zipUrl);
+  const filestream = fs.createWriteStream(path);
+  await new Promise((resolve, reject) => {
+    res.body.pipe(filestream);
+    res.body.on("error", reject);
+    filestream.on("finish", resolve);
+  });
+};
+
+// const sendOutput = async (
+//   submissionUrl: string,
+//   message: unknown
+// ): Promise<string> => {
+//   const response = await fetch(submissionUrl, {
+//     method: "POST",
+//     headers: {
+//       Accept: "application.json",
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(message)
+//   });
+
+//   const res = await response.text();
+//   return res;
+// };
